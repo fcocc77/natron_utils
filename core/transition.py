d15c6f499401b40d196df7899ca881356fc72643
Francisco Contreras <fcocc77@gmail.com>
2020-05-09 15:23:25 -0400

    Flare Transition

--- core/transition.py
+++ core/transition.py
@@ -3,18 +3,48 @@ import NatronEngine
 def main(thisParam, thisNode, thisGroup, app, userEdited):
     knob_name = thisParam.getScriptName()
 
-    param = thisNode.Dissolve1.which
-    param = thisNode.Transform2.translate
+    set_transition(thisNode)
 
+def set_transition(thisNode):
     duration = thisNode.duration.get()
     exaggeration_time = thisNode.exaggeration.get()
     exaggeration_value = thisNode.exaggeration_value.get()    
     start_frame = thisNode.start_frame.get()
+    blur_size = thisNode.blur_transition.get()
     values = [ thisNode.value_range.getValue(0), thisNode.value_range.getValue(1) ]
 
-    transition(param, duration, exaggeration_time, exaggeration_value, start_frame, values)
+    dissolve = thisNode.Dissolve1.which
+    flare_merge = thisNode.flare_merge.mix
+    blur = thisNode.Blur.size
 
-def transition(param, duration, exaggeration_time, exaggeration_value, start_frame, values):
+    directional_transition(dissolve, duration, exaggeration_time, exaggeration_value, start_frame, values)
+    back_and_forth_transition(flare_merge, duration, start_frame, [0, 1] )
+    back_and_forth_transition(blur, duration, start_frame, [0, blur_size] )
+
+def back_and_forth_transition(param, duration, start_frame, values):
+    # transicion ida y vuelta
+
+    first_frame = start_frame
+    last_frame = first_frame + duration
+    central_frame = ( first_frame + last_frame ) / 2
+
+    value_a = values[0]
+    value_b = values[1]    
+
+    for dimension in range( param.getNumDimensions() ):
+        param.restoreDefaultValue(dimension)
+
+        param.setValueAtTime(value_a, first_frame, dimension)
+        param.setValueAtTime(value_b, central_frame, dimension)
+        param.setValueAtTime(value_a, last_frame, dimension)
+
+        horizontal = NatronEngine.Natron.KeyframeTypeEnum.eKeyframeTypeHorizontal
+        param.setInterpolationAtTime(first_frame,  horizontal, dimension)
+        param.setInterpolationAtTime(central_frame,  horizontal, dimension)
+        param.setInterpolationAtTime(last_frame,  horizontal, dimension)
+
+
+def directional_transition(param, duration, exaggeration_time, exaggeration_value, start_frame, values):
     
     exaggeration_value = 1 - exaggeration_value  
     
