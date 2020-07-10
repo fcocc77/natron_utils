db8fcfadd6160469d4821605846afe529b1e0067
Francisco Contreras <fcocc77@gmail.com>
2020-07-10 17:23:07 -0400

    init.py correccion

--- core/init.py
+++ core/init.py
@@ -2,13 +2,16 @@ import os
 
 path = '/usr/share/Natron/Plugins'
 ignore = ['init', 'initGui']
+ignore_folders = ['tools']
 
 # Carga todos los modulos
 for root, dirs, files in os.walk(path):
     for f in files:
         name = f.split('.')[0]
         ext = f.split('.')[-1]
+        folder_name = os.path.basename(root)
 
-        if not name in ignore:
-            if ext == 'py':
-                exec('import ' + name)
+        if not folder_name in ignore_folders:
+            if not name in ignore:
+                if ext == 'py':
+                    exec('import ' + name)
