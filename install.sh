3f274e9a00c96047b1abf767e025892ee8d0f896
Francisco Contreras <fcocc77@gmail.com>
2020-07-17 12:10:04 -0400

    cambio de ruta de destino para vinarender

--- install.sh
+++ install.sh
@@ -1,22 +1,24 @@
 #! /usr/bin/sh
 cd $(dirname "$0") # Directorio Raiz
 
-function install() {
-    mkdir -p $1
-    rm -rf $1/*
-
-    # copia el core a la carpeta plugins de natron
-    cp -rf ./core $1
-    cp -rf ./plugins $1
-    cp -rf ./vinarender $1
-    cp -rf ./testing $1
-
-    # inserta la ruta de la instancia a init.py
-    init="$1/core/init.py"
-    sed -i "s|{path}|$1|g" $init
-
-    chmod 777 -R $1
-}
-
 plugins="/usr/share/Natron/Plugins"
-install "$plugins"
+vinarender="/opt/vinarender/modules/natron"
+
+mkdir -p $plugins
+rm -rf $plugins/*
+
+mkdir -p $vinarender
+rm -rf $vinarender/*
+
+# copia el core a la carpeta plugins de natron
+cp -rf ./core $plugins
+cp -rf ./plugins $plugins
+
+cp -rf ./vinarender/* $vinarender
+cp -rf ./testing/* $vinarender
+
+# inserta la ruta de la instancia a init.py
+init="$plugins/core/init.py"
+sed -i "s|{path}|$plugins|g" $init
+
+chmod 777 -R $plugins
