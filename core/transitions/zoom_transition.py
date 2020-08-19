e1f2ab82cb37280c01999bcd71b14fd7efd55ba9
Francisco Contreras <fcocc77@gmail.com>
2020-08-19 16:20:29 -0400

    zoom transition ok

--- core/transitions/zoom_transition.py
+++ core/transitions/zoom_transition.py
@@ -1,6 +1,5 @@
 import NatronEngine
 from nx import getNode
-# from base import *
 from animations import exaggerated_animation
 
 
@@ -18,14 +17,36 @@ def refresh(thisNode):
 
     dissolve = thisNode.Dissolve1.which
     start_frame = thisNode.getParam('start_frame').get()
+    rscale = thisNode.getParam('rscale').get()
+
     duration = thisNode.getParam('duration').get()
 
-    exaggerated_animation(dissolve, duration, start_frame, [0.0, 1.0])
+    # animacion para el dissolve solo con la mitad de la duracion
+    mid_duration = duration / 2
+    start_frame_dissolve = start_frame + (mid_duration / 2)
+    exaggerated_animation(dissolve, mid_duration, start_frame_dissolve, [0.0, 2.0])
+
+    src_blur = getNode(thisNode, 'src_blur')
+    src_blur_param = src_blur.getParam('size')
+    blur_size = thisNode.getParam('blur').get() * 2 * rscale
+    exaggerated_animation(src_blur_param, duration, start_frame, [0, blur_size])
 
     src_transform = getNode(thisNode, 'src_transform')
-    src_scale = src_transform.getParam('scale')
-    exaggerated_animation(src_scale, duration, start_frame, [1.0, 0.5], key_frames=[True, True])
+
+    src_rotate_param = src_transform.getParam('rotate')
+    src_rotate = thisNode.getParam('src_rotate').get()
+    exaggerated_animation(src_rotate_param, duration, start_frame, [0.0, src_rotate])
+
+    src_scale_param = src_transform.getParam('scale')
+    src_scale = thisNode.getParam('src_scale').get()
+    exaggerated_animation(src_scale_param, duration, start_frame, [1.0, src_scale])
 
     dst_transform = getNode(thisNode, 'dst_transform')
-    dst_scale = dst_transform.getParam('scale')
-    exaggerated_animation(dst_scale, duration, start_frame, [2.0, 1.0], key_frames=[True, True])
+
+    dst_rotate_param = dst_transform.getParam('rotate')
+    dst_rotate = thisNode.getParam('dst_rotate').get()
+    exaggerated_animation(dst_rotate_param, duration, start_frame, [dst_rotate, 0])
+
+    dst_scale_param = dst_transform.getParam('scale')
+    dst_scale = thisNode.getParam('dst_scale').get()
+    exaggerated_animation(dst_scale_param, duration, start_frame, [dst_scale, 1.0])
