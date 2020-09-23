6130a58301bcada789dc456184db351e67c1541d
Francisco Contreras <fcocc77@gmail.com>
2020-09-23 09:43:16 -0300

    format y speed switch: init

---
+++ core/merge/speed_switch.py
@@ -0,0 +1,18 @@
+from base import link_to_parent
+from nx import getNode
+
+
+def main(thisParam, thisNode, thisGroup, app, userEdited):
+    if not userEdited:
+        return
+
+    knob_name = thisParam.getScriptName()
+    link_to_parent(thisNode, thisParam, thisGroup)
+
+    if knob_name == 'refresh':
+        refresh(thisNode)
+
+
+def refresh(thisNode):
+    switch = getNode(thisNode, 'switch').getParam('which')
+    switch.set(thisNode.speed.get())
