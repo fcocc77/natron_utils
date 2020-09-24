07b7eb0ce32956af05c53a81f2efc3da154cdb7e
Francisco Contreras <fcocc77@gmail.com>
2020-09-24 18:52:52 -0300

    text glitch: init

---
+++ core/texts/text_glitch.py
@@ -0,0 +1,24 @@
+from base import link_to_parent, children_refresh, get_rscale, get_duration, get_format, get_start_frame
+from nx import getNode
+from text_fit import refresh_text_fit
+
+
+def main(thisParam, thisNode, thisGroup, app, userEdited):
+    if not userEdited:
+        return
+
+    knob_name = thisParam.getScriptName()
+    link_to_parent(thisNode, thisParam, thisGroup)
+    children_refresh(thisParam, thisNode)
+
+    if knob_name == 'refresh':
+        refresh(thisNode)
+
+
+def refresh(thisNode):
+    rscale = get_rscale(thisNode)
+    duration = get_duration(thisNode)
+    width, height = get_format(thisNode)
+    start_frame = get_start_frame(thisNode)
+
+    title_node, subtitle_node = refresh_text_fit(thisNode)
