9d2fa339a03701e0c36bbbb7ca92a3dffb641a20
Francisco Contreras <fcocc77@gmail.com>
2020-10-16 12:31:50 -0300

    __template__ : children refresh despues

--- core/common/__template__.py
+++ core/common/__template__.py
@@ -7,11 +7,12 @@ def main(thisParam, thisNode, thisGroup, app, userEdited):
         return
 
     knob_name = thisParam.getScriptName()
-    link_to_parent(thisNode, thisParam, thisGroup)
-    children_refresh(thisParam, thisNode)
 
     if knob_name == 'refresh':
         refresh(thisNode)
+
+    link_to_parent(thisNode, thisParam, thisGroup)
+    children_refresh(thisParam, thisNode)
 
 
 def refresh(thisNode):
